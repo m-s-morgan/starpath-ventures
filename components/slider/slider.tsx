540e498913da17799574a3bf38eb0d612c76fc1a
Matt Morgan <m@getplatypi.com>
2021-04-09 18:51:31 -0400

    swapping out keen slider

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -1,44 +1,21 @@
 import React from 'react';
-import { useKeenSlider } from 'keen-slider/react';
-import 'keen-slider/keen-slider.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from './slider.module.css';
 
 export default function Slider(props: ISliderProps) {
-    const auto = props.auto;
-    const [sliderRef, slider] = useKeenSlider({
-        controls: !auto,
-        loop: true,
-        duration: 2000,
-    });
-
-    if (auto) {
-        React.useEffect(() => {
-            const timeout = setInterval(() => {
-                if (!!slider) {
-                    slider.next();
-                }
-            }, 6000);
-
-            return () => {
-                clearInterval(timeout);
-            };
-        }, [slider]);
-    }
-
     return (
-        <>
-        <div ref={sliderRef as React.RefObject<HTMLDivElement>} className={`keen-slider ${styles.slider}`}>
+        <Carousel autoPlay={props.auto} infiniteLoop={true} swipeable={false} interval={5000} showArrows={false} showStatus={false} showThumbs={false} transitionTime={1500} stopOnHover={false}>
             {
                 props.slides.map((s, i) => {
                     return (
-                        <div key={i} className="keen-slider__slide">
+                        <div key={i} className={styles.slide}>
                             <h1 className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl ${styles.header}`}>{s}</h1>
                         </div>
                     );
                 })
             }
-        </div>
-        </>
+        </Carousel>
     );
 }
 
